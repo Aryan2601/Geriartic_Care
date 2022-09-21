b3735be823d4a51600d86db319b6f1219129b2bd
Arya R N <aryakunjamma@gmail.com>
2022-09-21 10:38:49 +0530

    added activity for caretaker login and registration

---
+++ app/src/main/java/com/project/geriarticcare/authentication/CaretakerLoginPage.java
@@ -0,0 +1,26 @@
+package com.project.geriarticcare.authentication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.geriarticcare.R;
+
+public class CaretakerLoginPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caretaker_login);
+
+        final TextView CaretakerRegisterNow = findViewById(R.id.CaretakerRegister);
+        CaretakerRegisterNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CaretakerLoginPage.this, CaretakerRegistrationPage.class));
+            }
+        });
+    }
+}
