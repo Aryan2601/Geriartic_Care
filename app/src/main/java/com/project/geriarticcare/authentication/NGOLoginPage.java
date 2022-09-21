ceee4bed70e1a95444e3affa0079ee2c798d6eb7
Arya R N <aryakunjamma@gmail.com>
2022-09-21 11:10:24 +0530

    Added activity for NGO login and registration

---
+++ app/src/main/java/com/project/geriarticcare/authentication/NGOLoginPage.java
@@ -0,0 +1,13 @@
+package com.project.geriarticcare.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NGOLoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/project/geriarticcare/authentication/NGORegistrationPage.java
@@ -0,0 +1,13 @@
+package com.project.geriarticcare.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NGORegistrationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
